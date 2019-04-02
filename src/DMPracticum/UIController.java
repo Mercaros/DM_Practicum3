2a26b85454d394ea4e181dadd2e17335012d0ec1
Daan Dirker <Daan.development@gmail.com>
2019-04-02 16:08:48 +0200

    Added an UI flow start

--- src/DMPracticum/UIController.java
+++ src/DMPracticum/UIController.java
@@ -1,2 +1,49 @@
+package DMPracticum;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 public class UIController {
+
+    private String resultEncryptionStepOne;
+    private String resultEncryptionStepTwo;
+    private String resultEncryptionStepThree;
+
+    private String resultDecryptionStepOne;
+    private String resultDecryptionStepTwo;
+
+    @FXML private TextField sentence;
+    @FXML private TextArea encryptionResultOne;
+
+    @FXML
+    private void encryptionStepOne() {
+        if (sentence == null) {
+            return;
+        }
+
+        this.resultEncryptionStepOne = calculateEncryptionStepOne();
+        encryptionResultOne.setText(this.resultEncryptionStepOne);
+    }
+
+    private String calculateEncryptionStepOne() {
+        return sentence.getText();
+    }
+
+    @FXML
+    private void encryptionStepTwo() {
+    }
+
+    @FXML
+    private void encryptionStepThree() {
+    }
+
+    @FXML
+    private void decryptionStepOne() {
+    }
+
+    @FXML
+    private void decryptionStepTwo() {
+    }
+
 }
