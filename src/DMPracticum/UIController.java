3acccf7dd053f98b084fcff1dddbe3585373f321
Daan Dirker <Daan.development@gmail.com>
2019-04-02 16:37:04 +0200

    Expanded Controller for encryption

--- src/DMPracticum/UIController.java
+++ src/DMPracticum/UIController.java
@@ -1,6 +1,8 @@
 package DMPracticum;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
@@ -14,28 +16,64 @@ public class UIController {
     private String resultDecryptionStepTwo;
 
     @FXML private TextField sentence;
-    @FXML private TextArea encryptionResultOne;
+
+    @FXML private Button stepTwoEncryptionButton;
+    @FXML private Button stepThreeEncryptionButton;
+
+    @FXML private TextArea encryptionResultOneText;
+    @FXML private TextArea encryptionResultTwoText;
+    @FXML private TextArea encryptionResultThreeText;
 
     @FXML
     private void encryptionStepOne() {
-        if (sentence == null) {
+        //Other two button will be disabled when running for a second time
+        disableEncryptionButtons();
+
+        if (sentence == null || sentence.getText().isEmpty()) {
+            showMissingInputError();
             return;
         }
 
         this.resultEncryptionStepOne = calculateEncryptionStepOne();
-        encryptionResultOne.setText(this.resultEncryptionStepOne);
+        encryptionResultOneText.setText(this.resultEncryptionStepOne);
+        stepTwoEncryptionButton.setDisable(false);
     }
 
     private String calculateEncryptionStepOne() {
-        return sentence.getText();
+        String result = sentence.getText();
+        return "The result is: " + result;
     }
 
     @FXML
     private void encryptionStepTwo() {
+        this.resultEncryptionStepTwo = calculateEncryptionStepTwo();
+        stepThreeEncryptionButton.setDisable(false);
+    }
+
+    private String calculateEncryptionStepTwo() {
+        return "";
     }
 
     @FXML
     private void encryptionStepThree() {
+        this.resultEncryptionStepThree = calculateEncryptionStepThree();
+
+    }
+
+    private String calculateEncryptionStepThree() {
+        return "";
+    }
+
+    private void disableEncryptionButtons() {
+        stepTwoEncryptionButton.setDisable(true);
+        stepThreeEncryptionButton.setDisable(true);
+    }
+
+    private void showMissingInputError() {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Error");
+        alert.setHeaderText("Input is required!");
+        alert.showAndWait();
     }
 
     @FXML
