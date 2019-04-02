d90cdaa648e092ba99b4eb1680d5b5c84f2101a9
Daan Dirker <Daan.development@gmail.com>
2019-04-02 15:12:52 +0200

    Fixed naming issue

--- src/DMPracticum/Main.java
+++ src/DMPracticum/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("src/test/DMPracticum/UI.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("UI.fxml"));
         primaryStage.setTitle("Discrete Mathematics Encryption/Decryption");
         primaryStage.setScene(new Scene(root, 1037, 570));
         primaryStage.show();
