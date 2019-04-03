90d7fe97faf6dee47f4a19f6d60ca2d6ba298593
Unknown <khaled_t23@hotmail.com>
2019-04-04 00:47:41 +0200

    Layout changes + start on decryption

--- src/DMPracticum/UIController.java
+++ src/DMPracticum/UIController.java
@@ -6,7 +6,15 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
+import java.math.BigInteger;
+
+import static java.math.BigInteger.ONE;
+
 public class UIController {
+
+    private BigInteger encryptedMessage = new BigInteger("28546624436443611514125411254163066528125416306482212965276777916528790077856306272812903483811622630627281296565282767662129031151463061286948386306272812965652827676621290311514630648226528128692296306790048386306276748381254112541630648382728630611514790012869229662125416528129651254111622471963062495101846528790012541128694838790063069681229662129034436229652827672767");
+    private BigInteger n, d, e;
+    private BigInteger p, q;
 
     private String resultEncryptionStepOne;
     private String resultEncryptionStepTwo;
@@ -24,7 +32,12 @@ public class UIController {
     @FXML private TextArea encryptionResultTwoText;
     @FXML private TextArea encryptionResultThreeText;
 
-    @FXML
+    @FXML private TextField decryptionSentenceN;
+    @FXML private TextField decryptionSentenceE;
+    @FXML private TextField decryptionSentenceC;
+    @FXML private TextArea decryptionResultOne;
+    @FXML private TextArea decryptionResultTwo;
+
     private void encryptionStepOne() {
         //Other two button will be disabled when running for a second time
         disableEncryptionButtons();
@@ -78,6 +91,13 @@ public class UIController {
 
     @FXML
     private void decryptionStepOne() {
+        n = new BigInteger("13231");
+        e = new BigInteger("2");
+
+        BigInteger phi = (p.subtract(ONE)).multiply(q.subtract(ONE));
+
+        d = e.modInverse(phi);
+        decryptionResultOne.setText(String.valueOf(d));
     }
 
     @FXML
